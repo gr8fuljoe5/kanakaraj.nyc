814780b96d518fb39b1ccaa2e2e6285cf907e224
Joseph Kanakaraj <joe@Josephs-MacBook-Pro.local>
2014-11-30 12:36:34 -0500

    updates

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,36 @@
-'use strict';
-
 angular.module('joek', ['restangular', 'ui.router'])
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('home', {
+      .state('base', {
         url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
+        views : {
+          'header' : {
+            templateUrl : 'components/navbar/navbar.html',
+            controller : ['$scope','$log',function($scope,$log){
+                $log.log('header loaded');
+            }]
+          },
+          'footer' : {
+            template: '<div>{{text}}}</div>',
+            controller: ['$scope', '$log', function($scope,$log){
+              $scope.text = 'FOOTER GOES HERE!';
+            }]
+          },
+          'main' : {
+            templateUrl : 'app/main/main.html',
+            controller: ['$scope', '$log', function($scope,$log){
+              $scope.main = 'header';
+              $log.log('main section initiated');
+            }]
+          }
+        }
+      })
+      .state('base.about', {
+        url: 'about',
+        templateUrl: 'app/about/about.html',
+        controller: function($log){
+            $log.log('MAIN CONTROLLER INITIATED');
+        }
       });
 
     $urlRouterProvider.otherwise('/');
