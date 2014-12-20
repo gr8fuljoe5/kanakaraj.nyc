95fa8ea76d9c459e0b2db0c476c2940fabdc2d05
Joseph Kanakaraj <joe@Josephs-MacBook-Pro.local>
2014-12-20 16:30:58 -0500

    clean controller

--- src/app/index.js
+++ src/app/index.js
@@ -38,48 +38,6 @@ angular.module('joek', ['restangular', 'ui.router', 'angularLoad'])
         controller: function($log){
           $log.log('skillset CONTROLLER INITIATED');
 
-          var data = [90];
-
-          var width = 520,
-            barHeight = 40;
-
-          var x = d3.scale.linear()
-            .domain([0, d3.max(data)])
-            .range([0, width]);
-
-          var chart = d3.select(".toolbox-chart")
-            .attr("width", width)
-            .attr("height", barHeight * data.length);
-
-
-
-          var xAxis = d3.svg.axis()
-            .scale(x)
-            .orient("bottom")
-            .ticks(10, "%");
-
-          var bar = chart.selectAll("g")
-            .data(data)
-            .enter().append("g")
-            .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; })
-
-
-          bar.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + barHeight + ")")
-            .call(xAxis);
-
-          bar.append("rect")
-            .attr("width", x)
-            .attr("height", barHeight - 1);
-
-          bar.append("text")
-            .attr("x", function(d) { return x(d) - 3; })
-            .attr("y", barHeight / 2)
-            .attr("dy", ".35em")
-            .text(function(d) { return d; });
-
-
         }
       }).state('base.recommendations', {
         url: 'recommendations',
