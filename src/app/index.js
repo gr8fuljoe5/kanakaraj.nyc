2ace24c0c46216173d6863128c6e5566a0a6fc21
Joseph Kanakaraj <joe@Josephs-MacBook-Pro.local>
2014-12-20 18:26:19 -0500

    skillset updates

--- src/app/index.js
+++ src/app/index.js
@@ -35,9 +35,84 @@ angular.module('joek', ['restangular', 'ui.router', 'angularLoad'])
       .state('base.skillset', {
         url: 'skillset',
         templateUrl: 'app/skillset/skillset.html',
-        controller: function($log){
+        controller: function($log, $interval, $scope){
           $log.log('skillset CONTROLLER INITIATED');
 
+          var quotes = [
+            {"text": "Most good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to program.",
+              "author" : "Linus Torvalds"},
+
+            {"text": "Those people who think they know everything are a great annoyance to those of us who do.",
+              "author" : "Isaac Asimov"},
+
+            {"text": "The true sign of intelligence is not knowledge but imagination.",
+              "author" : "Albert Einstein"},
+
+            {"text": "No man's knowledge here can go beyond his experience.",
+              "author" : "John Locke"},
+
+            {"text": "People who work together will win, whether it be against complex football defenses, or the problems of modern society.",
+              "author": "Vince Lombardi"}
+          ];
+
+          function generateQuote(){
+            var randomNum = Math.floor(Math.random() * quotes.length)
+            var elem = document.querySelector('.quote');
+            var quote = '<div class="animated fadeIn">' + quotes[randomNum].text + '<cite>' + quotes[randomNum].author + '</cite></div>';
+            //$log.log(quotes[randomNum].text + '<cite>' + quotes[randomNum].author + '</cite>');
+            elem.innerHTML = '';
+            elem.innerHTML = quote;
+          }
+          generateQuote();
+          $interval(function(){
+            //$log.log('interval');
+            generateQuote();
+
+
+          },10000);
+
+
+
+
+          var colors = [
+            '132,112,255',
+            '100,149,237',
+            '176,224,230',
+            '85,107,47',
+            '244,164,96',
+            '255,140,0',
+            '148,0,211',
+            '255,215,0',
+            '46,139,87'
+          ];
+          var chart = d3.selectAll('.skillset-chart')
+            .attr('width', '200')
+            .attr('height', '15')
+            .style('background-color', 'rgb(200, 200, 196)');
+
+          chart.each(function(d,i){
+            var thisChart = d3.select(this);
+            $log.log(i);
+
+
+            var attrs = {
+              'stroke' : 'none',
+              'fill' : 'rgb('+colors[i]+')',
+              'width': this.getAttribute('data-range') * 2,
+              'height':'15',
+              'x': '0',
+              'y': '0',
+              'rx' : '3',
+              'ry' : '3'
+            };
+            thisChart.append('rect')
+              .attr('width', 0)
+              .attr('fill','rgb(30,144,255)')
+              .transition()
+              .delay(3000)
+              .duration(1500)
+              .attr(attrs)
+          });
         }
       }).state('base.recommendations', {
         url: 'recommendations',
